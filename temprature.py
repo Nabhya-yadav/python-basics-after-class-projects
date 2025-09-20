9d2a76b6ea811c64d8632b5548848b13f2f02059
Nabhya-yadav <nabhya.yadav20@gmail.com>
2025-09-20 18:13:56 +0530

    Add files via upload

---
+++ temprature.py
@@ -0,0 +1,11 @@
+temp=int(input("Enter the temprature "))
+
+
+if temp <6:
+    print("Wear very warm clothes")
+
+elif temp <15:
+    print("Wear warm clothes")
+
+else:
+    print("wear light clothes")
