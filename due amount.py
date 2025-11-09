73d0c22ea7de86de70444d6379d3b6aec97e5527
Nabhya-yadav <nabhya.yadav20@gmail.com>
2025-11-09 21:03:47 +0530

    Add files via upload

---
+++ due amount.py
@@ -0,0 +1,6 @@
+totalamount=int(input("Enter the total amount"))
+paidamount=int(input("Enter the paid amount"))
+
+def subtract (totalamount,paidamount):
+    return totalamount-paidamount
+print(subtract(totalamount,paidamount))
